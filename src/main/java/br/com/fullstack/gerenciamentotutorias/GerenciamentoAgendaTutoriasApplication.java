1a0f4a2189aec3065161157cd9b0e3886a7ecacd
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-14 17:15:13 -0300

    Initial commit

---
+++ src/main/java/br/com/fullstack/gerenciamentotutorias/GerenciamentoAgendaTutoriasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.gerenciamentotutorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAgendaTutoriasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoAgendaTutoriasApplication.class, args);
+    }
+
+}
